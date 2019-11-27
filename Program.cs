ca0244b24635caf545cb600f062674122cb8e81c
MerveAksu <58237280+MerveAksu@users.noreply.github.com>
2019-11-27 14:17:21 +0300

    XMLReference
    Taking the following XML document, write code to extract the RefText values for the following RefCodes:   ‘MWB’, ‘TRV’ and ‘CAR’

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetDeclarationXMLReferenceList();
+        }
+
+        public static void GetDeclarationXMLReferenceList()
+        {
+            string path = $"{AppDomain.CurrentDomain.BaseDirectory}Declaration.xml";
+            List<string> listOfResult = new List<string>();
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+
+            XmlNodeList xml = doc.SelectNodes("/InputDocument/DeclarationList/Declaration/DeclarationHeader/Reference");
+            foreach (XmlNode node in xml)
+            {
+                var refCode = node.Attributes["RefCode"].InnerText;
+
+                switch (refCode)
+                {
+                    case "MWB":
+                        listOfResult.Add($"MWB - {node.FirstChild.InnerText}");
+                        break;
+                    case "TRV":
+                        listOfResult.Add($"TRV - {node.FirstChild.InnerText}");
+                        break;
+                    case "CAR":
+                        listOfResult.Add($"CAR - {node.FirstChild.InnerText}");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            foreach (var item in listOfResult)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
